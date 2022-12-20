45b137cf6aca573b436b760d75d1e26d5c84467a
dongyeop han <pig8776@naver.com>
2022-12-20 15:44:41 +0900

    22-12-20 url update

--- backend/config/express.js
+++ backend/config/express.js
@@ -11,8 +11,8 @@ module.exports = function () {
   app.use(express.urlencoded({ extended: true })); // form으로 제출되는 값 파싱
   app.use(methodOverride()); // put, delete 요청 처리
   app.use(cors()); // 웹 브라우저 cors 설정을 관리
-  app.use(express.static("/home/ubuntu/FoodMap/frontend"));
-  // app.use(express.static(process.cwd() + '/public'));
+  // app.use(express.static("/home/ubuntu/FoodMap/frontend"));
+  // app.use(express.static(process.cwd() + "/public"));
 
   /* 직접 구현해야 하는 모듈 */
   require("../src/routes/indexRoute")(app);
